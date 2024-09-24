6297cc0111e80f7fb0d7ef35fb04b1ed7683e4ff
fnmarcos1104 <marcosfnf@hotmail.com>
2024-09-24 18:40:08 -0300

    Aula05

---
+++ Aula05/Atividades_01.py
@@ -0,0 +1,20 @@
+import os
+
+os.system('cls')
+
+print('0 - Fim')
+print('1 - Inclui')
+print('2 - Altera')
+print('3 - Exclui')
+print('4 - Consulta')
+
+opc = int(input('Escolha uma das opções: '))
+
+if opc == 0:
+    print('Opcão 0, Fim!')
+elif == 1:
+    print('Opcão 1, Inclui!')
+ if == 2:
+     print('Opcão 2, Altera!')
+  else:
+    print('fim')
